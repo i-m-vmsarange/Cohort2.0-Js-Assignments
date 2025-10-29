73de1f31cfba119a1484f2901c727b88572fa0a6
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2025-10-29 16:27:18 +0530

    Completed Day-28 Assignment

---
+++ Day28-Js-Assignments/script.js
@@ -0,0 +1,84 @@
+// Js Assignments
+// alert("Hello from sheriyans coding school!");
+// let ans = prompt("If html is skeleton, css is clothes then what is js?");
+// document.writeln("Js " + ans);
+// document.writeln(
+//   "The example of js being used in website is when we click on buttion we can perform some action like changing the styles of the elements in the document."
+// );
+// alert("Hi");
+// prompt("How are you?");
+// let city = "Bhopal";
+// var a = "vaishnavi";
+// let b;
+// const c = "rachels";
+// a = "updated";
+// b = "updated";
+// // c = "updated";
+// let x = {
+//   id: 1,
+// };
+// console.log(x);
+// let name = "Vaishnavi";
+// let age = 23;
+// let city = "Pune";
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.warn(city);
+// console.info(age);
+// console.error(city);
+
+// let username = prompt("Enter your name");
+// alert("Welcome to sheryians coding school " + username);
+// console.log(typeof username);
+// let age = prompt("Enter your age");
+// console.log(age - 5);
+// let msg = "I love sheryians";
+// console.log(msg.slice(2, 6)); // love
+// console.log(msg.split());
+// console.log(msg.replace("love", "study at"));
+// let username = prompt("Enter name:");
+// let msg = "I love sheriyans!";
+// if (msg.includes("love")) {
+//   console.log("msg includes love");
+// } else {
+//   console.log("msg does include love");
+// }
+// vaishnavi
+// console.log("Vaishnavi");
+// my code prints my name to the console
+// let y = (5 + 10) * 2;
+// console.log(y);
+// let age = 25;
+// console.log(`type of age is ${typeof age}`);
+// let name = "Harsh";
+// console.log(`type of name is ${typeof name}`);
+// let isStudent = true;
+// console.log(`type of isStudent is ${typeof isStudent}`);
+// let skills = ["Js", "HTML"];
+// console.log(`type of skills is ${typeof skills}`);
+// let user = { city: "Bhopal" };
+// console.log(`type of user is ${typeof user}`);
+// let x = null;
+// console.log(`type of x is ${typeof x}`);
+// let y;
+// y = "Vishal";
+// console.log(`type of y is ${typeof y}`);
+// let z = Symbol("id");
+// z = 123;
+// console.log(`type of z is ${typeof z}`);
+// console.log(1 / 0);
+// console.log(0 / 0);
+// console.log(-1 / 0);
+// When initially we don't know the value to be given to the object we should give the value to object as null
+// When we declare a variable and don't initialise with a value the javascript itself gives default value as undefined
+// console.log(Number("abc"));
+// console.log(undefined + 1);
+// let x = 5;
+// let y = x;
+// y = 10;
+// console.log(x, y);
+// let obj1 = { name: "Harsh" };
+// let obj2 = obj1;
+// obj2.name = "vaishnavi";
+// console.log(obj1.name);

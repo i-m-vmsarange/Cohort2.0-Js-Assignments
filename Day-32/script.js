c74814636259b2379c2bcc2018eccf80607aa482
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2025-11-04 20:26:48 +0530

    Day-32

---
+++ Day-32/script.js
@@ -0,0 +1,20 @@
+//
+// for (let i = 0; i < 11; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + " EVEN");
+//   } else {
+//     console.log(i + " ODD");
+//   }
+// }
+// let number = +prompt("Enter a number:");
+
+// if (number > 0) {
+//   console.log(number + " is positive");
+// } else if (number === 0) {
+//   console.log(number + " is zero");
+// } else {
+//   console.log(number + " is negative");
+// }
+let age = Number(prompt("Ener your age:"));
+console.log(age);
+console.log(typeof age);
